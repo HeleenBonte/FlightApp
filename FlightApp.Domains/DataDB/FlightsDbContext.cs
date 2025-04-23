f5ec9acdce3ec45210df5dee480dd1c9f2536ec1
HeleenBonte <bonteheleen@hotmail.com>
2025-04-23 14:41:02 +0200

    DB Online gezet en connection strings aangepast

--- FlightApp.Domains/DataDB/FlightsDbContext.cs
+++ FlightApp.Domains/DataDB/FlightsDbContext.cs
@@ -50,7 +50,7 @@ public partial class FlightsDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQL22_VIVES; Database=Flights; Trusted_Connection=True; TrustServerCertificate=True; MultipleActiveResultSets=true;");
+        => optionsBuilder.UseSqlServer("Server=flightfullstack.database.windows.net; Initial Catalog=Flights; User ID = Beheerder; Password = FullstackServer4Me; Trusted_Connection=True; TrustServerCertificate=True; MultipleActiveResultSets=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
