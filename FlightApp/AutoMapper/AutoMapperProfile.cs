0bb0eca4bf36cd4657202ee93f590c5a93ec06d6
HeleenBonte <bonteheleen@hotmail.com>
2025-04-27 11:31:27 +0200

    Automapper toegevoegd en gedeeltelijk aangemaakt

---
+++ FlightApp/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using FlightApp.Domains.EntitiesDB;
+using FlightApp.ViewModels;
+
+namespace FlightApp.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //Flight
+            CreateMap<Flight, FlightVM>().ForMember(dest => dest.DepartureCity, 
+                opts => opts.MapFrom(
+                    src => src.DepartureCityNavigation.CityName))
+                .ForMember(dest => dest.ArrivalCity,
+                opts => opts.MapFrom(
+                    src => src.ArrivalCityNavigation.CityName));
+
+
+
+            //Booking
+            CreateMap<Booking, BookingVM>()
+                .ForMember(dest => dest.UserName,
+                    opts => opts.MapFrom(
+                        src => src.User.UserName))
+                .ForMember(dest => dest.RouteId,
+                    opts => opts.MapFrom(
+                        src => src.Route.RouteId));
+
+
+            //City
+            CreateMap<City, CityVM>();
+
+
+            // CreateMap<Source, Destination>();
+            // CreateMap<Destination, Source>();
+            // CreateMap<Source, Destination>().ReverseMap();
+            // CreateMap<Source, Destination>().ForMember(dest => dest.Property, opt => opt.MapFrom(src => src.Property));
+        }
+    }
+    
+}
