f3a10c18d75f5487f0848e8dabdae56c0689fc6e
HeleenBonte <bonteheleen@hotmail.com>
2025-05-08 17:15:23 +0200

    added seating to flightvm

--- FlightApp/ViewModels/FlightVM.cs
+++ FlightApp/ViewModels/FlightVM.cs
@@ -10,7 +10,8 @@ namespace FlightApp.ViewModels
         public DateTime? DepartureTime { get; set; }
         public DateTime? ArrivalTime { get; set; }
         public double? Price { get; set; }
-        public IEnumerable<TicketVM>? Tickets { get; set; }
+        public IEnumerable<Ticket>? Tickets { get; set; } = new List<Ticket>();
+        public int seating { get; set; }
 
     }
 }
